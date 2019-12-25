a03532f50201abff1a4830c2e8d1ecec2a517466
celebrated-integer-2023 <scailles23@gmail.com>
2019-12-25 23:27:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+
+number=
+
+def prime?(number)
+  divisors=["2","3"]
+  result= divisors.each do |divisor|
+    number%#{divisor}
+  end
+  if result.any?= TRUE
+    puts result.to_s
+  elsif result=FALSE
+  puts "Not an integer"
+end
+end
