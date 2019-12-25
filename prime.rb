094bb1a81c7f68f15b88e9076463789d22159220
celebrated-integer-2023 <scailles23@gmail.com>
2019-12-25 23:44:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ number=
 
 def prime?(number)
 divsors=(2..(number-1)).to_a
-if divsors.any? {|i| number/i==0}||integer==1||integer==1
+if divsors.any? {|i| number/i==0||integer==1||integer==1}
   FALSE
 else
   true
