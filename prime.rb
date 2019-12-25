479386e2bb6d2a8eb8eee0b4be3b511394582e4f
celebrated-integer-2023 <scailles23@gmail.com>
2019-12-25 23:46:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,3 +4,4 @@ def prime?(integer)
   test_divisors = (2..(integer - 1)).to_a	  test_divisors = (2..(integer - 1)).to_a
   if test_divisors.any? {|divisor| integer % divisor == 0} || integer == 0 || integer == 1 	  (test_divisors.any? {|divisor| integer % divisor == 0} || integer == 0 || integer == 1) ? false : true
 end
+end
