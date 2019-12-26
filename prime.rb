e03f4fb57b7c1c7abb4fcdca829863e83cb60dbe
celebrated-integer-2023 <scailles23@gmail.com>
2019-12-26 00:49:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 # Add  code here!
-number=[11]
+number=[]
 
 def prime?(number)
 
@@ -7,7 +7,7 @@ if number<=1
 return false
 elsif number<=3
 return true
-else (2..n - 1).any? {|x| n%x!= 0}
+else (2..n - 1).any? {|x| number%x!= 0}
 end
 
 end
