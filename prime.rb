09a7a7423f5f5bf5116b26969144608e6e7e18e7
celebrated-integer-2023 <scailles23@gmail.com>
2019-12-26 00:53:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,7 +7,8 @@ if number<=1
 return false
 elsif number<=3
 return true
-else (2..number - 1).any? {|x| number%x!= 0}:{|x| number%x!<=0}
+else (2..number - 1).any? {|x| number%x!= 0}
+else false
 end
 
 end
