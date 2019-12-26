e4128439ad6addd9f501ca86f67f3a06cea7b14a
celebrated-integer-2023 <scailles23@gmail.com>
2019-12-26 01:09:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,13 @@
 # Add  code here!
 number=[]
 
+def prime?(number)
+  if number <= 1
+    return false
+  elsif n <= 3
+    return true
+  else (2..n/2).none? do |x|
+    number % x == 0
+  end
+  end
+end
