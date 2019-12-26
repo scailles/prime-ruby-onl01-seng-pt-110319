7894a54fffa6ef77b52171b60aee91edaeab3030
celebrated-integer-2023 <scailles23@gmail.com>
2019-12-26 01:11:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+number=[]
+
+def prime?(number)
+  if number <= 1
+    return false
+  elsif number <= 3
+    return true
+  else (2..number/2).none? {|x| number % x == 0}
+  end
+end
