484e1e221a5204f51f3e3040976f0ccdc7106581
celebrated-integer-2023 <scailles23@gmail.com>
2019-12-26 01:08:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,15 +3,4 @@ number=[]
 
 def prime?(number)
 
-case result||number
-
-when number <=1 
-  return FALSE
-  
-when number<=3
-  return TRUE
-  
-when (2..number - 1).none? {|x| number%x!= 0}
-  
-end
 end
